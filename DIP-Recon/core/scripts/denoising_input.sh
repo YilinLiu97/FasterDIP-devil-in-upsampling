402747e956087bd83e56176036fa202645ab91d3
Yilin Liu <yilinliu@email.unc.edu>
2023-10-15 13:52:40 -0400

    script for changing the input to an image

---
+++ DIP-Recon/core/scripts/denoising_input.sh
@@ -0,0 +1,46 @@
+python train_unsup.py --task denoising \
+                      --folder_path '../data/denoising' \
+                      --save_folder '/mnt/yaplab/data/yilinliu/saves/CBM3D/input_noise' \
+                      --input_image_name 'image_Baboon512rgb.png' \
+                      --imsize -1 \
+                      --model_type DIP_2_scaled \
+                      --progressive False \
+                      --num_iters 3000 \
+                      --norm_func bn \
+                      --noise_sigma 25 \
+                      --filter_size_down 3 \
+                      --filter_size_up 3 \
+                      --num_skips 0 \
+                      --need_sigmoid True \
+                      --loss_func mse \
+                      --freq_loss_func moment_matching \
+                      --num_scales 2 \
+                      --optimizer adam \
+                      --decay_lr False \
+                      --step_size 50 \
+                      --gamma 0.55 \
+                      --morph_lbda 1e-5 \
+                      --min_tau 0.4 \
+                      --exp_weight 0.99 \
+                      --Lipschitz_constant 0 \
+                      --Lipschitz_reg 0 \
+                      --verbose False \
+                      --num_power_iterations 0 \
+                      --noise_type u \
+                      --noise_method noisy_image \
+                      --special 2levels_0skips_64chns_NoisyBaboonForF16\
+                      --prune_type None \
+                      --reg_type 0 \
+                      --decay 1e-5 0 \
+                      --freq_lbda 0 \
+                      --prune_type None \
+                      --reg_type 0 \
+                      --decay 1e-7 0 \
+                      --freq_lbda 0 \
+                      --reg_noise_std '1/30'\
+                      --act_func ReLU \
+                      --upsample_mode 'bilinear' \
+                      --pad zero \
+                      --dim 64 \
+                      --lr 0.01              
+            
